46072ee49b489360cc58c17175e299a59b61db35
Richard Clarke <31780296+rgclarke@users.noreply.github.com>
2024-01-17 15:04:11 +0800

    Update Products sidebar (Fix)
    Fix comment-out

--- src/modules/products/components/product-tabs/index.tsx
+++ src/modules/products/components/product-tabs/index.tsx
@@ -49,32 +49,18 @@ const ProductInfoTab = ({ product }: ProductTabsProps) => {
         <div className="flex flex-col gap-y-4">
           <div>
             <span className="font-semibold">Grape</span>
-            <p>{product.material ? product.material : "-"}</p>
+            <p>{product.material ? product.material : ""}</p>
           </div>
           <div>
             <span className="font-semibold">Country of origin</span>
-            <p>{product.origin_country ? product.origin_country : "-"}</p>
+            <p>{product.origin_country ? product.origin_country : ""}</p>
           </div>
           <div>
             <span className="font-semibold">Type</span>
-            <p>{product.type ? product.type.value : "-"}</p>
+            <p>{product.type ? product.type.value : ""}</p>
           </div>
         </div>
-        {/* <div className="flex flex-col gap-y-4">
-          <div>
-            <span className="font-semibold">Weight</span>
-            <p>{product.weight ? `${product.weight} g` : "-"}</p>
-          </div>
-          <div>
-            <span className="font-semibold">Dimensions</span>
-            <p>
-              {product.length && product.width && product.height
-                ? `${product.length}L x ${product.width}W x ${product.height}H`
-                : "-"}
-            </p>
-          </div>
-        </div>
-      </div> */}
+      </div>
       {product.tags?.length ? (
         <div>
           <span className="font-semibold">Tags</span>
@@ -93,8 +79,10 @@ const ShippingInfoTab = () => {
           <div>
             <span className="font-semibold">Fast delivery</span>
             <p className="max-w-sm">
-              Standard shipping in Hong Kong within 3 business days. See checkout for express and other options. Free delivery with minimum order.
-              Or you can pick up your order from our warehouse at any time during business hours.
+              Standard shipping in Hong Kong within 3 business days. See
+              checkout for express and other options. Free delivery with minimum
+              order. Or you can pick up your order from our warehouse at any
+              time during business hours.
             </p>
           </div>
         </div>
@@ -103,7 +91,8 @@ const ShippingInfoTab = () => {
           <div>
             <span className="font-semibold">Exchanges</span>
             <p className="max-w-sm">
-              Unopened merchandise may be returned for a full refund within 7 days &#40;excluding Sale and Bin-end items&#41;.
+              Unopened merchandise may be returned for a full refund within 7
+              days &#40;excluding Sale and Bin-end items&#41;.
             </p>
           </div>
         </div>
@@ -113,8 +102,8 @@ const ShippingInfoTab = () => {
             <span className="font-semibold">Corked Wine</span>
             <p className="max-w-sm">
               If your wine has a cork closure and has spoiled, just return the
-              bottle with a substantial amount of the contents remaining for an exchange
-              or refund.
+              bottle with a substantial amount of the contents remaining for an
+              exchange or refund.
             </p>
           </div>
         </div>
@@ -122,5 +111,20 @@ const ShippingInfoTab = () => {
     </div>
   )
 }
+
+// ;<div className="flex flex-col gap-y-4">
+//   <div>
+//     <span className="font-semibold">Weight</span>
+//     <p>{product.weight ? `${product.weight} g` : "-"}</p>
+//   </div>
+//   <div>
+//     <span className="font-semibold">Dimensions</span>
+//     <p>
+//       {product.length && product.width && product.height
+//         ? `${product.length}L x ${product.width}W x ${product.height}H`
+//         : "-"}
+//     </p>
+//   </div>
+// </div>
 
 export default ProductTabs
